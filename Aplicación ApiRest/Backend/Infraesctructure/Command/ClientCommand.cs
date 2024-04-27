64669aff564ff27afd04d815e5defe2057f9d4ac
Nicolas Vitek <71182488+NicolasVitek@users.noreply.github.com>
2024-04-27 11:52:23 -0300

    Add files via upload

---
+++ Aplicación ApiRest/Backend/Infraesctructure/Command/ClientCommand.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interface;
+using Domain.Entities;
+using Infraesctructure.Persistence;
+
+namespace Infraesctructure.Command
+{
+    public class ClientCommand: IClientCommand
+    {
+        private readonly AppDbContext _context;
+
+        public ClientCommand(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task InsertClient(Cliente cliente)
+        {
+            _context.Add(cliente);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Aplicación ApiRest/Backend/Infraesctructure/Command/OrderCommand.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interface;
+using Domain.Entities;
+using Infraesctructure.Persistence;
+
+namespace Infraesctructure.Command
+{
+    public class OrderCommand: IOrderCommand
+    {
+        private readonly AppDbContext _context;
+
+        public OrderCommand(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task InsertOrder(Orden order)
+        {
+             _context.Add(order);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
