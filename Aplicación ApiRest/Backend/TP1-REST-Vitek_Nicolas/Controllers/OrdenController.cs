a025322c815a6fb70e39c81ab4f6cb6b30bb53db
Nicolas Vitek <71182488+NicolasVitek@users.noreply.github.com>
2024-04-27 11:59:08 -0300

    Add files via upload

---
+++ Aplicación ApiRest/Backend/TP1-REST-Vitek_Nicolas/Controllers/OrdenController.cs
@@ -0,0 +1,52 @@
+﻿using Application.Exceptions;
+using Application.Interface;
+using Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace TP1_REST_Vitek_Nicolas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdenController : ControllerBase
+    {
+        private readonly IOrderService _service;
+
+        public OrdenController(IOrderService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("{clientId}")]
+        public async Task<IActionResult> CreateOrder(int clientId)
+        {
+            try
+            {
+                var result = await _service.CreateOrder(clientId);
+                return new JsonResult(result) { StatusCode = 201 };
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest("Ese cliente no tiene un carrito ingresado");
+            }
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> ShowBalance([FromQuery]DateTime from, [FromQuery] DateTime to)
+        {
+            try
+            {
+                if (from==to)
+                {
+                    throw new WrongDate();
+                }
+                var result = await _service.ShowBalance(from, to);
+                return new JsonResult(result);
+            }
+            catch (WrongDate ex)
+            {
+                return BadRequest(ex.message);
+            }
+        }
+    }
+}
