5eb94fb034900521cd0203a6a12523dcae275894
Nicolas Vitek <nicolasvitek91@hotmail.com>
2024-06-28 15:31:08 -0300

    [NEW] Test finished.

--- Java web application/Backend/src/test/java/com/example/demo/ClientServiceTest.java
+++ Java web application/Backend/src/test/java/com/example/demo/ClientServiceTest.java
@@ -4,36 +4,39 @@ import com.example.demo.services.ClientService;
 import com.example.demo.repositories.IClientRepositoy;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import org.junit.runner.RunWith;
 import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ClientServiceTest {
-    
-    @Mock
-    private IClientRepositoy clientRepository;
-
-    @InjectMocks
+    @Autowired
     private ClientService clientService;
-    private Client client;
+
+    @Autowired
+    private IClientRepositoy clientRepository;
     
     @Before
     public void setUp() {
-        client = new Client();
+        clientRepository.deleteAll();
+        Client client = new Client();
+        client.setId(1);
         client.setUserName("Nicolas");
-        when(clientRepository.getByUserName("Nicolas")).thenReturn(client);
+        clientRepository.save(client);
     }
     @Test
     public void testGetClientByUserName() {
         Client result = clientService.getClientByUserName("Nicolas");
         assertNotNull(result);
         assertEquals("Nicolas", result.getUserName());
-        verify(clientRepository).getByUserName("Nicolas");
+    }
+    @After
+    public void tearDown() {
+        clientRepository.deleteAll();
     }
 }
